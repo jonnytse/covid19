c7fc1a4b12c691a495a81dd96527479f3cac2e89
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-06-03 15:13:06 +0100

    the array of data from the API is getting console logged correctly

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -5,14 +5,21 @@ import SearchBar from '../../components/SearchBar';
 
 class App extends Component {
   //getCountry is a method
-  getCountry = (e) => {
+  getCountry = async (e) => {
     const country = e.target.elements.country.value;
     e.preventDefault();
+    const api_call = await fetch(`https://api.covid19api.com/summary`);
+    // const api_call = await fetch('https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search');
+    // const api_call = await fetch('https://api.covid19uk.live/');
     console.log(country);
+
+    const data = await api_call.json();
+    console.log(data); //this is an object
+    console.log(data.Countries); //this is the array
   }
 
   render() {
-    return (
+    return ( 
       <div className={styles.app}>
         <Header />
         <SearchBar getCountry={this.getCountry} />
