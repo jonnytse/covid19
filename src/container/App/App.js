a1efe42d62715d6ffae6eb3bd4efd46cbd042a0d
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-06-03 15:25:33 +0100

    added this.setState in App.js to be able to console.log the countries upon Search button click, instead of purely console logging the API data without setting the state

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -4,18 +4,24 @@ import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
 
 class App extends Component {
+  state = {
+    //create an empty array that stores whatever is requested from the API
+    countries: []
+  }
+
+
   //getCountry is a method
   getCountry = async (e) => {
     const country = e.target.elements.country.value;
     e.preventDefault();
     const api_call = await fetch(`https://api.covid19api.com/summary`);
-    // const api_call = await fetch('https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search');
-    // const api_call = await fetch('https://api.covid19uk.live/');
     console.log(country);
 
     const data = await api_call.json();
     console.log(data); //this is an object
-    console.log(data.Countries); //this is the array
+    // console.log(data.Countries); //this is the array
+    this.setState({ countries: data.Countries });
+    console.log(this.state.countries);
   }
 
   render() {
